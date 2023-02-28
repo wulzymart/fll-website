9280b64ef03b1f70c65416d4c2f238babb7089e7
OlawoleMartins <olawalemart@gmail.com>
2023-02-28 15:38:41 +0100

    hot fix

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -16,7 +16,7 @@ function TestimonialCarousel({ testimonials }) {
     <div className="py-5 px-5 md:px-20 bg-slate-200">
       <h3 className="text-3xl font-bold mb-2 text-gray-700 ">Our Reviews</h3>
       <p className="">
-        Dont just take our words, Here what other clients are saying
+        Dont just take our words, Hear what other clients are saying
       </p>
       <div className="pt-20 pb-10 w-[90%] mx-auto">
         <Slider {...settings}>
